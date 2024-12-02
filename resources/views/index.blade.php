e1332bb3b897aa494ffb421e3ce9a3f6bec1566a
alanprayogo <alanbp98@gmail.com>
2024-12-03 00:58:04 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="main-header">
+            <h4>Dashboard</h4>
+        </div>
+    </div>
+
+    <!-- statistics -->
+    <div class="row dashboard-header">
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Employee</span>
+                <h2 class="dashboard-total-products">4500</h2>
+                <span class="label label-primary">Views</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-id-badge text-primary-color"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Customer</span>
+                <h2 class="dashboard-total-products">37,500</h2>
+                <span class="label label-warning">Views</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-user text-warning-color"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Variant Product</span>
+                <h2 class="dashboard-total-products">$<span>30,780</span></h2>
+                <span class="label label-danger">Views</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-direction-alt text-danger-color"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Products</span>
+                <h2 class="dashboard-total-products">$<span>30,780</span></h2>
+                <span class="label label-info">Views</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-package text-info-color"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row dashboard-header">
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Sales</span>
+                <h2 class="dashboard-total-products">4500</h2>
+                <span class="label label-primary">Sales</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-wallet text-primary-color"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Order</span>
+                <h2 class="dashboard-total-products">37,500</h2>
+                <span class="label label-warning">Sales</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-shopping-cart text-warning-color"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Stock Available</span>
+                <h2 class="dashboard-total-products">$<span>30,780</span></h2>
+                <span class="label label-danger">Views</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-archive text-danger-color"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card dashboard-product">
+                <span>Delivery</span>
+                <h2 class="dashboard-total-products">$<span>30,780</span></h2>
+                <span class="label label-success">Sales</span><a href="">More Info</a>
+                <div class="side-box">
+                    <i class="ti-truck text-success-color"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end statistics -->
+@endsection
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,48 @@
+<aside class="main-sidebar hidden-print">
+    <section class="sidebar" id="sidebar-scroll">
+        <!-- Sidebar Menu-->
+        <ul class="sidebar-menu">
+            <li class="nav-level">--- Admin</li>
+            <li class="active treeview">
+                <a class="waves-effect waves-dark" href="index.html">
+                    <i class="icon-speedometer"></i><span> Dashboard</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="waves-effect waves-dark" href="user-management.html">
+                    <i class="icofont icofont-user"></i><span> User Management</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="waves-effect waves-dark" href="role-access.html">
+                    <i class="icofont icofont-lock"></i><span> Role & Access Management</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="waves-effect waves-dark" href="catalog-management.html">
+                    <i class="icofont icofont-tag"></i><span> Catalog Management</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="waves-effect waves-dark" href="stock-management.html">
+                    <i class="icofont icofont-box"></i><span> Stock Management</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="waves-effect waves-dark" href="order-delivery.html">
+                    <i class="icofont icofont-cart"></i><span> Order & Delivery</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="waves-effect waves-dark" href="sales-transactions.html">
+                    <i class="icofont icofont-wallet"></i><span> Sales & Transactions</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="waves-effect waves-dark" href="reports-analytics.html">
+                    <i class="icofont icofont-pie-chart"></i><span> Reports & Analytics</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+</aside>
